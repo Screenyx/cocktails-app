21204e12ba6319065ee6a5856f462ae52d4fe66b
Screenyx <mercier.hugo14@gmail.com>
2021-11-06 14:52:08 +0100

    app with default detail and list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cocktail } from './cocktail';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cocktails';
+  title = 'app works !';
+
+  cocktails = new Cocktail('Mojito', 'http://anotherwhiskyformisterbukowski.com/wp-content/uploads/2016/09/mojito-1.jpg', "Le mojito, prononcé en espagnol, est un cocktail à base de rhum, de citron vert et de feuilles de menthe fraîche, né à Cuba dans les années 1910.");
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CocktailsListComponent } from './cocktails-list/cocktails-list.component';
+import { CocktailsDetailsComponent } from './cocktails-details/cocktails-details.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CocktailsListComponent,
+    CocktailsDetailsComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
